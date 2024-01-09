0ec31c942f8dabd527452ee03c81750a9d14bfdb
gongahkia <angryapplegravy@gmail.com>
2024-01-09 22:37:56 +0800

    some changes added for now

---
+++ README.md
@@ -0,0 +1,25 @@
+![Static Badge](https://img.shields.io/badge/skill_hunter_1.0-passing-green)
+
+# Skill Hunter
+
+Browser extension that formats SSO legislation to be more readable.
+
+![](https://i.redd.it/531lsuu5cj081.jpg)
+
+## Motivation
+
+## Purpose
+
+## Screenshots
+
+## Installation
+
+### CLI
+
+```console
+
+```
+
+### GUI
+
+1. 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 // FUA 
-    // continue adding code where i stopped --> restructure how data is stored
+    // restructure how data is stored
+    // work out how to preserve tabs and newlines and existing formatting
     // further check what other page information needs to be extracted
-    // work out general structure of SSO website
+    // general structure of SSO website
         // each section title and its text are nested within a div classes are provXHdr and provTxtX where X is a number that increases
-        // use regex?
+        // each provTxtX should check whether it starts with a number, if it does and not a bracket, then indent the portion otherwise dont indent
+        // each provTxtX should then be under the provHdrX section with an indent
     // implement parser in a main.js file first
     // add later popups and other functions seperately
+    // allow reformatting to include a gruvbox theme
 
 // 2 implement
-    // rename this project nicely
-    // get actual good assets for the icon pictures
     // when this project's parsing is done, add this to git
     // make frontend layout like CCLAW L4 project with dynamic responsive tables
     // add a make file as required
@@ -19,9 +20,16 @@
 
 alert("walahi");
 
+// ---------- PARSING ----------
+
 var pageData = {
-    pageTitle:"",
-    pageBody:{}
+    statuteTitle:"",
+    statuteBody:[]
+}
+
+var section = {
+    sectionTitle:"",
+    sectionBody:"",
 }
 
 var regex = /^prov\d+(Txt|Hdr)$/; // matching regex for prov1Hdr or prov1Txt
@@ -30,13 +38,26 @@ var txtElements = [];
 var hdrElements = [];
 Array.from(elements).forEach(function(element) {
     if (regex.test(element.className)) {
-        if (element.className.endsWith('Txt')) {
-            txtElements.push(element);
-        } else if (element.className.endsWith('Hdr')) {
-            hdrElements.push(element);
+        if (element.className.endsWith('Hdr')) {
+            if (section.sectionTitle === "") {
+                section.sectionTitle = element.textContent.trim();
+            } else {
+                pageData.statuteBody.push(section);
+                section = {
+                    sectionTitle:"",
+                    sectionBody:"",
+                }
+                section.sectionTitle = element.textContent.trim();
+            }
+        } else if (element.className.endsWith('Txt')) {
+            section.sectionBody += element.textContent.trim();
         }
+    } else if (element.className === "legis-title") {
+        pageData.statuteTitle = element.textContent.trim();
     }
 });
+pageData.statuteBody.push(section);
 
-txtElements.forEach(textEl => console.log(textEl.textContent));
-hdrElements.forEach(headEl => console.log(headEl.textContent));
+console.log(pageData);
+
+// --------- REFORMATTING ----------
